73bc89481d09f3e086bfcfe4c7d169e9de73363f
Johandielangman <jghanekom2@gmail.com>
2025-03-01 13:06:15 +0200

    remove prerendering

---
+++ amplify/custom-functions/helloworld/index.py
@@ -0,0 +1,9 @@
+
+
+def handler(event, context):
+    print("Event: ", event)
+
+    return {
+        'statusCode': 200,
+        'body': 'Hello from Lambda!'
+    }
--- src/routes/about/+page.server.js
+++ src/routes/about/+page.server.js
@@ -11,5 +11,3 @@ export function load({ cookies }) {
 		visited: visited === 'true'
 	};
 }
-
-export const prerender = true;
