30eaabf27a552a0f84b765885555d1c676ed2ee1
Johandielangman <jghanekom2@gmail.com>
2025-03-01 16:47:06 +0200

    backend build

--- src/routes/lambda/+page.server.js
+++ src/routes/lambda/+page.server.js
@@ -1,8 +1,4 @@
-import { Amplify } from "aws-amplify"
 import { generateClient } from "aws-amplify/api"
-import outputs from "../../../amplify_outputs.json"
-
-Amplify.configure(outputs)
 
 export async function load() {
     const client = generateClient()
